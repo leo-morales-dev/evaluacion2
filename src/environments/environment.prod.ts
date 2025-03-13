780e331ed6c2da254919f75bcd57da99a2963439
leo-morales-dev <leonardo.morales.work@gmail.com>
2025-03-12 22:32:46 -0600

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+      apiKey: "AIzaSyDbCVwF3j8b-9zOF0igjar2JBddGulQWok",
+      authDomain: "evaluacion-266bc.firebaseapp.com",
+      projectId: "evaluacion-266bc",
+      storageBucket: "evaluacion-266bc.firebasestorage.app",
+      messagingSenderId: "358669783265",
+      appId: "1:358669783265:web:997b79f17ec1298e6767a0",
+    },
+  };
+  
